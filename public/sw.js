40c60f233cd4d00c40e6525dab4643ad56b9c496
pance24-web <3.09369903e+08+pance24-web@users.noreply.github.com>
2026-09-08 14:09:55 +0000

    fix: refresh inline chord layout assets

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-// PowerChord Service Worker - v11
-const CACHE_VERSION = 'powerchord-v11';
+// PowerChord Service Worker - v12
+const CACHE_VERSION = 'powerchord-v12';
 const STATIC_CACHE = `static-${CACHE_VERSION}`;
 const DATA_CACHE = `data-${CACHE_VERSION}`;
 
